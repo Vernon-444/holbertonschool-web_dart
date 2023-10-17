fe7060f528d47894ef8bfac9b9e0732daed6d77e
Brayden Vernon <mt.vernon44@gmail.com>
2023-10-16 21:33:05 -0500

    update task 4

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,14 +1,10 @@
-/**
-* Write two functions the first function outer() take two argument name, id and print the output of the inside function inner().
-*
-* Function prototype : void outer(String name, String id)
-* Function prototype : String inner()
-* name will always consist of two words
-*/
+void outer(String name, String id) {
+    print(inner(name, id));
+}
 
-void outer(String name, String id){
-    String inner(){
-        return "My name is $name and my id is $id";
-    }
-    print(inner());
+String inner(String name, String id) {
+    List<String> words = name.split(' ');
+    String firstWord = words[0];
+    String firstLetterSecondWord = words[1][0];
+    return 'Hello Agent $firstLetterSecondWord.$firstWord your id is $id';
 }
