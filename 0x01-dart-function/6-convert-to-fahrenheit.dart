bc28a648a080c1d083a19975e4ce6f4402e2a985
Brayden Vernon <mt.vernon44@gmail.com>
2023-10-16 21:24:13 -0500

    adjust formula for correct approx

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -1,7 +1,7 @@
 // accepts a list of doubles representing list of temps in celcius
 // returns a list of doubles representing the same temps in fahrenheit
 
-List<double> convertToF(List<double> celsiusTemps) {
+List<double> convertToF(List<double> temperaturesInC) {
   // return a list of doubles representing the temps in fahrenheit rounded to 1 decimal place
-  return celsiusTemps.map((temp) => double.parse((temp * 1.8 + 32).toStringAsFixed(1))).toList();
+  return temperaturesInC.map((celsius) => double.parse((celsius * 9 / 5 + 32).toStringAsFixed(2))).toList();
 }
